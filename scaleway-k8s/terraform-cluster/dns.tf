524536b324765fdb1f67925eb9c5bc245fe4e75a
Martin McCaffery <martin.mccaffery@think-ahead.tech>
2024-06-24 15:01:01 +0200

    Use terraform-created IP address for Kubernetes LB

---
+++ scaleway-k8s/terraform-cluster/dns.tf
@@ -0,0 +1,23 @@
+resource "scaleway_lb_ip" "teleport" {}
+
+resource "scaleway_domain_record" "teleport" {
+  dns_zone = "thinkahead.dev"
+  name     = "teleport"
+  type     = "A"
+  data     = scaleway_lb_ip.teleport.ip_address
+  ttl      = 60
+}
+
+resource "scaleway_secret" "kubernetes-cluster-ip" {
+  name = "lb-ip"
+  path = "/teleport-demo/kubernetes"
+}
+
+resource "scaleway_secret_version" "kubernetes-cluster-ip" {
+  secret_id = scaleway_secret.kubernetes-cluster-ip.id
+  data = scaleway_lb_ip.teleport.ip_address
+}
+
+output "fixed-lb-ip" {
+    value = scaleway_lb_ip.teleport.ip_address
+}
--- scaleway-k8s/terraform-cluster/provider.tf
+++ scaleway-k8s/terraform-cluster/provider.tf
@@ -24,6 +24,6 @@ provider "scaleway" {
   access_key = var.SCW_ACCESS_KEY
   secret_key = var.SCW_SECRET_KEY
   project_id = var.SCW_DEFAULT_PROJECT_ID
-  zone       = "fr-par-2"
+  zone       = "fr-par-1"
   region     = "fr-par"
 }
